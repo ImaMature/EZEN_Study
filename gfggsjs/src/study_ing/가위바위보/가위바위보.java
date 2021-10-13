18fbf866d6a573dd0ecaf0768a2e9097e99ca78c
ImaMature <ImaMature 88884623+ImaMature@users.noreply.github.com>
2021-10-13 18:45:24 +0900

    가위바위보 복습

---
+++ gfggsjs/src/study_ing/가위바위보/가위바위보.java
@@ -0,0 +1,12 @@
+package study_ing.가위바위보;
+
+import java.util.Scanner;
+
+public class 가위바위보 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		
+	}
+}
