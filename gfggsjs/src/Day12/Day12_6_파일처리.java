a5300f6fa147cdb7758182c508993c38616225cc
ImaMature <ImaMature 88884623+ImaMature@users.noreply.github.com>
2021-10-15 15:04:10 +0900

    이클립스에서 파일출력 스트림 이용 -> txt파일을 로컬폴더에 생성하기

---
+++ gfggsjs/src/Day12/Day12_6_파일처리.java
@@ -0,0 +1,15 @@
+package Day12;
+
+public class Day12_6_파일처리 {
+	//파일 처리
+	
+	//메모리 [주기억장치 = RAM] : 변수, 배열, 객체 등등 : 프로그램 종료시 다 초기화됨
+		// 주기억장치의 역할 : 현재 실행중인 프로그램 [ 명령어 집합 ] 기억
+		// 보조기억장치 : 하드디스크
+	
+	public static void main(String[] args) {
+		
+		//파일처리 클래스
+		
+	} //main end	
+}//class end
