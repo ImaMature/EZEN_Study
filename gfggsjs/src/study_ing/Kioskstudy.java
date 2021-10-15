7af0f801a27ad8ee943c61d1bc7f98f4deda77a2
ImaMature <ImaMature 88884623+ImaMature@users.noreply.github.com>
2021-10-15 11:33:03 +0900

    키오스크 복습 수정

--- gfggsjs/src/study_ing/Kioskstudy.java
+++ gfggsjs/src/study_ing/Kioskstudy.java
@@ -52,7 +52,7 @@ public class Kioskstudy {
 			System.out.println("품목\t개수\t가격(개당)");
 			if(colaPurchase>0) {System.out.println("콜라\t"+colaPurchase+"\t"+(colaPurchase*300));}
 			if(fantaPurchase>0) {System.out.println("환타\t"+fantaPurchase+"\t"+(fantaPurchase*200));}
-			if(sodaPurchase>0) {System.out.println("사이다\t"+sodaPurchase+"\t"+(sodaPurchase*300));}
+			if(sodaPurchase>0) {System.out.println("사이다\t"+sodaPurchase+"\t"+(sodaPurchase*100));}
 			System.out.println("-------------------------");
 			System.out.println("총 결제 금액 : " + ((colaPurchase*300)+(fantaPurchase*200)+(sodaPurchase*100)));
 			System.out.println("-------------------------");
@@ -61,39 +61,39 @@ public class Kioskstudy {
 				if(sodaPurchase==0 && colaPurchase == 0 && fantaPurchase == 0) {
 					System.out.println("장바구니에 결제할 제품이 없습니다.\n");
 				}else {
+					
 					while (true) {
 						System.out.println("결제를 진행하시겠습니까? [y] [n]"); String ch2 = scanner.next(); 
 						if(ch2.equals("y")) {
 							System.out.println("결제를 진행합니다.");
+							
 							while(true) {
 								System.out.println("결제 금액을 투입해주세요."); int 지불할금액 = scanner.nextInt();
 								int 총금액 = ((colaPurchase*300)+(fantaPurchase*200)+(sodaPurchase*100));
 							//-------------------------------------------------------------------	
-								if(지불할금액<총금액) { // (사용자가 입력한 금액 < 지불해야하는 금액) 
-									System.out.println("잔액이 부족합니다.");
-								}else {
-									System.out.println("결제 완료!! 잔돈 : " + (지불할금액-총금액));
-									colaPurchase =0; fantaPurchase=0; sodaPurchase=0;
-									colaStock=10; 	fantaStock=10; 	sodaStock=10;
-									break; 
-								}
-							}break; 
-							//-----------------------------------------------------------------------
+//								if(지불할금액<총금액) { // (사용자가 입력한 금액 < 지불해야하는 금액) 
+//									System.out.println("잔액이 부족합니다.");
+//								}else {
+//									System.out.println("결제 완료!! 잔돈 : " + (지불할금액-총금액));
+//									colaPurchase =0; fantaPurchase=0; sodaPurchase=0;
+//									colaStock=10; 	fantaStock=10; 	sodaStock=10;
+//									break; 
+//								}
+//							}break; 
 							
-						/*		
 						  		// (사용자가 입력한 금액 >= 지불해야하는 금액)
 								if	(지불할금액 >= 총금액) { 
 									System.out.println("결제 완료!! 잔돈 : " + (지불할금액-총금액));
 									colaPurchase =0; fantaPurchase=0; sodaPurchase=0;
-									colaStock=10; fantaStock=10; sodaStock=10; 
+									colaStock=10; fantaStock=10; sodaStock=10; break;
 									
 								}else {	
 									System.out.println("잔액이 부족합니다.");
 									break;	
 								}
-							}break;에 오류가 발생 (해당 break;를 삭제하라고 뜸)
+							}break;//에 오류가 발생 (해당 break;를 삭제하라고 뜸)
 							
-						*/	
+						
 							
 						}else if (ch2.equals("n")) {
 							System.out.println("결제를 취소하고 이전화면으로 돌아갑니다.");
