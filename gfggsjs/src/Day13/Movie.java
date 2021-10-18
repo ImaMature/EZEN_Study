c6a0353838e8831f85ad0e0d9ee8c8b847ab5f30
ImaMature <ImaMature 88884623+ImaMature@users.noreply.github.com>
2021-10-18 15:27:46 +0900

    수정1

--- gfggsjs/src/Day13/Movie.java
+++ gfggsjs/src/Day13/Movie.java
@@ -5,8 +5,8 @@ public class Movie extends Thread{
 	static boolean stop = true;
 	public static void moviestop(boolean sw) {
 		stop = sw;
-		if(stop) { System.out.println("영화 시작");}
-		else {System.out.println("영화 종료");}
+		if(stop)	{ System.out.println("영화 시작");}
+		else 		{System.out.println("영화 종료");}
 	}
 	@Override
 	public void run() {
