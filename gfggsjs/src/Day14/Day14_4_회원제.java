480b08c00278fb1d8bd3333a09049272762e4cfc
ImaMature <ImaMature 88884623+ImaMature@users.noreply.github.com>
2021-10-19 16:25:16 +0900

    리스트를 이용한 회원제 프로그램(+파일처리)

--- gfggsjs/src/Day14/Day14_4_회원제.java
+++ gfggsjs/src/Day14/Day14_4_회원제.java
@@ -1,5 +1,7 @@
 package Day14;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,6 +15,39 @@ public class Day14_4_회원제 {
 		Scanner scanner = new Scanner(System.in);
 		//0-1. 설정 [리스트 생성] User 클래스만 담을 수 있는 클래스 (User 클래스의 하위도 가능)
 		ArrayList<User> users = new ArrayList<>(); //클래스 생성을 위해 users 임시객체 생성
+		//0-2. [파일 읽어오기 : 파일에 있는 회원들을 리스트에 저장]
+			//예외처리
+		try{
+			FileInputStream fileInputStream = new FileInputStream("C:/Users/505/git/EZEN_Study/gfggsjs/src/Day14/userlist.txt"); //, true 쓸 필요X
+		
+		
+			//0-2-1. 파일내 모든 글자 가져오기 [ 스트림 단위 : byte ]
+					//1) 바이트 배열 선언부터 해야함
+					byte[] bytes = new byte[1024]; // 1bit -> 8bit -> 1byte -> 1024byte - > 1kb -> 1024kb -> 1mb
+								//영문, 특수문자, 공백 1바이트		//한글 2바이트
+					//2) 읽어오기
+					fileInputStream.read(bytes);
+					//3) 바이트 -> 문자열 변환
+					String inString = new String(bytes);
+					
+			//0-2-2. 회원 구분하기 //split (기준) 분리 -> 배열
+					String [] usernum = inString.split("\n");
+			//0.2-3. 필드 구분하기
+					for(String temp: usernum) {
+						
+						String[] userdata = temp.split(","); //userdata에 저장되어있는 값들을" ," 으로 나눔
+						
+						if(userdata.length==1) break; //마지막 회원은 공백회원이라 1번부터 시작
+						//0-2-4. 객체 생성
+						User user = new User(userdata[0] , userdata[1] , userdata[2]);
+						//0-2-5. 리스트 담기
+						users.add(user);
+					}
+		}
+		catch(Exception e) {
+			System.out.println("[[파일 불러오기 실패 ]]");
+		}
+		
 		
 		//1.메뉴
 		while(true) {
@@ -36,8 +71,28 @@ public class Day14_4_회원제 {
 					}
 				}
 				//3. 리스트에 저장 [ 만약에 중복값이 없으면 ]
-				if(idcheck) {users.add(user); System.out.println("[[회원가입 성공]]");}// if 안에 세미콜론 하나다 그러면 {}스킵가능, 두개 이상이면 {} 필수
+				if(idcheck) {users.add(user); System.out.println("[[회원가입 성공]]");// if 안에 세미콜론 하나다 그러면 {}스킵가능, 두개 이상이면 {} 필수
 				
+				//4. 파일처리
+					//1. 파일 출력 클래스 (FileOutputStream fileOutputStream = new FileOutputStream("파일경로/파일명.확장자") 꼭 슬래쉬로 사용
+							//이어쓰기 하려면 확장자 뒤에 , true
+							//예외처리 필수
+						try {
+							FileOutputStream fileOutputStream = new FileOutputStream("C:/Users/505/git/EZEN_Study/gfggsjs/src/Day14/userlist.txt", true);
+																																		//누적은 맨뒤에 true
+							//2.필드값 구분 [,]					//회원간 구분					[\n]
+							String outString = user.getId()+", "+user.getPassword()+", "+user.getName()+"\n"; //쉼표나 백슬래시 n을 회원가입할 때 막아놔야됨
+							//3. String ---> byte 내보내기
+							fileOutputStream.write(outString.getBytes()); //바이트 형변환 꼭 필요.
+							
+							System.out.println("[[파일 처리 성공]]");
+							
+							
+						} catch (Exception e) {
+							System.out.println("[[파일 처리 실패]]");
+						}
+				
+				}
 			}else if(ch==2) {
 				//1. 입력받기
 				System.out.println("아이디 : ");		String id = scanner.next(); 
