be76824e8d43fc57f872a2ee37da6ba5f68e769f
ImaMature <ImaMature 88884623+ImaMature@users.noreply.github.com>
2021-10-20 15:56:17 +0900

    스택구조 이용해 문자열 역순 출력

---
+++ gfggsjs/src/Day15/Day15_문제4.java
@@ -0,0 +1,39 @@
+package Day15;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Day15_문제4 {
+/*
+ * 문제4 : 스택 구조를 이용한 문자열 역순 출력
+ * 	[조건1] Stack 구조
+ * 	[조건2] 입력받은 문자열을 순서대로 스택에 저장
+ * 		Char이나 String써도 됨
+ * 	[조건3] 문자열 반대로 출력
+ * 	[출력예시] 입력 : ABC
+ * 			 출력 : CBA
+ * 	//하나씩 빼오기
+ */
+	
+
+
+	public static void main(String[] args) {
+		Stack<Character> alphabet = new Stack<>();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("문자열 입력 : ");  String input = sc.next();
+		
+		
+		for(int i=0; i<input.length(); i++) {
+			alphabet.push(input.charAt(i)); //charAt = 문자열을 쪼개서 하나씩 쌓기
+		}
+		//입력값이 하나씩 잘나오는지 확인하기
+		System.out.println(alphabet);
+
+			// 꺼내기
+		for ( int i =0; i<input.length(); i++) {
+			System.out.print(alphabet.pop());
+		}
+		
+	}
+}
