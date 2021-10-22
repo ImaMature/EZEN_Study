0374233f5802c5136518864aa59795e948d489ab
ImaMature <ImaMature 88884623+ImaMature@users.noreply.github.com>
2021-10-22 09:35:50 +0900

    gf

--- gfggsjs/src/test/ReportCard.java
+++ gfggsjs/src/test/ReportCard.java
@@ -19,48 +19,42 @@ public class ReportCard {
 	ArrayList<Students> array = new ArrayList<>();
 		
 		while(true) {
-		try {
-			Scanner scanner = new Scanner(System.in);
-			System.out.println("1. 등록 | 2. 출력"); int ch = scanner.nextInt();
-			if(ch==1) {
-				
-				System.out.println("등록화면입니다. 정보를 입력해 주세요.");
-				System.out.print("이름 : "); String name = scanner.next();
-				System.out.print("국어 : "); int kor_Grade = scanner.nextInt();
-				System.out.print("영어 : "); int eng_Grade = scanner.nextInt();
-				System.out.print("수학 : "); int math_Grade = scanner.nextInt();
-
-				int sum = (kor_Grade+eng_Grade+math_Grade);
-				double average = sum/3;
-				Students a = new Students(name, kor_Grade, eng_Grade, math_Grade, sum, average);
-				
-				array.add(a);				
+			try {
+				Scanner scanner = new Scanner(System.in);
+				System.out.println("1. 등록 | 2. 출력"); int ch = scanner.nextInt();
+				if(ch==1) {
+					
+					System.out.println("등록화면입니다. 정보를 입력해 주세요.");
+					System.out.print("이름 : "); String name = scanner.next();
+					System.out.print("국어 : "); int kor_Grade = scanner.nextInt();
+					System.out.print("영어 : "); int eng_Grade = scanner.nextInt();
+					System.out.print("수학 : "); int math_Grade = scanner.nextInt();
 	
-				System.out.println(a);
-
-			}else if (ch==2) {
-				System.out.println("출력 화면입니다.");
-				System.out.println("----------------------------------------------------");
-				System.out.println("\t\t  성    적    표");
-				System.out.println("----------------------------1------------------------");
-				System.out.println("번호\t이름\t국어\t영어\t1수학\t총점\t평균\t석차");
-				System.out.println("----------------------------------------------------");
-				int i = 1;
-				Collections.sort(array);
-				for(Students c : array) {
-					System.out.printf(i+"\t%s\t%d\t%d\t%d\t%d\t%.2f\t%d", c.getName(), c.getKor(),
-							c.getEng(),c.getMath(),c.getSum(),c.getAverage(),i);
-					System.out.println(); i++;
-				}
-				
-				}
-		}catch(Exception e) {System.out.println("잘못된 입력입니다. 관리자한테 가서 따지세요.");}
-				
-				
-				
-			
-			
-			
+					int sum = (kor_Grade+eng_Grade+math_Grade);
+					double average = sum/3;
+					Students a = new Students(name, kor_Grade, eng_Grade, math_Grade, sum, average);
+					
+					array.add(a);				
+		
+					System.out.println(a);
+	
+				}else if (ch==2) {
+					System.out.println("출력 화면입니다.");
+					System.out.println("----------------------------------------------------");
+					System.out.println("\t\t  성    적    표");
+					System.out.println("----------------------------1------------------------");
+					System.out.println("번호\t이름\t국어\t영어\t1수학\t총점\t평균\t석차");
+					System.out.println("----------------------------------------------------");
+					int i = 1;
+					Collections.sort(array);
+						for(Students c : array) {
+							System.out.printf(i+"\t%s\t%d\t%d\t%d\t%d\t%.2f\t%d", c.getName(), c.getKor(),
+									c.getEng(),c.getMath(),c.getSum(),c.getAverage(),i);
+							System.out.println(); i++;
+						}
+					System.out.println("----------------------------------------------------");
+					}
+			}catch(Exception e) {System.out.println("잘못된 입력입니다. 관리자한테 가서 따지세요.");}
 		}//while end
 	}//main end
 }//class end
