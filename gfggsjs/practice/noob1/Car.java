800aab4c457ee29d5706cf6505be4d69b03211b5
ImaMature <ImaMature 88884623+ImaMature@users.noreply.github.com>
2021-10-28 10:36:59 +0900

    클래스 상속 연습

---
+++ gfggsjs/practice/noob1/Car.java
@@ -0,0 +1,9 @@
+package noob1;
+
+public class Car {
+
+	Tire frontLeftTire = new Tire (10, "앞왼쪽"); 
+	// Tire클래스의 생성자에 인수값을 넘겨준다.
+	// 그럴려면 Tire 클래스에 해당 객체가 들어갈만한 생성자를 만들고 해야된다.
+	
+}
--- gfggsjs/practice/noob1/Tire.java
+++ gfggsjs/practice/noob1/Tire.java
@@ -1,25 +1,47 @@
 package noob1;
 
-import java.util.Scanner;
 
-public class a1 {
+	public class Tire {
+	
+		/*
+		 * Day09_2, Tire, KumhoTire, hanKookTire, car 클래스클 연습
+		 *  상속
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+	int maxWheel; // 최대 회전 수
+	int acWheel; //현재 회전 수
+	String location; // 타이어 위치
+		
+	
 
-	/*
-	 * 회원 ( 시간 객체 )
-	 * 키오스크
-	 * 관리자
-	 *  
-	 * 
-	 * 
-	 * 
-	 * 
-	 */
+	public Tire(int maxWheel, int acWheel, String location) {
+		super();
+		this.maxWheel = maxWheel;
+		this.acWheel = acWheel;
+		this.location = location;
+	}
 	
 	
-	public static Scanner scanner = new Scanner(System.in);
+
 	
-	public static void main(String[] args) {
-	
+
+	public Tire(int Wheel, String location) { 
+		// 생성자를 만들때 다른사람이 알아볼수 있도록 필드값과 받을 매개변수명을 통일해야함.
+		// 그리고 어떤걸 받을 건지 고심해봐야됨.
+		super();
+		this.acWheel = acWheel;
+		this.location = location;
+	}
+
+
+
+
+
+	public void roll() {
 		
 	}
+	
 }
